94d170415a7a6c01cb097a91752e2613811cfdf0
Aksh-007 <akshaybarapatre54@gmail.com>
2023-12-11 12:08:10 +0530

    feat - new Assingment data binding added

--- src/Component/Dashboard/UserSidebar/Assignment/Newassignment.jsx
+++ src/Component/Dashboard/UserSidebar/Assignment/Newassignment.jsx
@@ -8,16 +8,76 @@ import {
   Stack,
   Text,
 } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 import { useNavigate } from "react-router-dom";
 
 // import React from "react";
 
-
 // import "./EmployeeProfileEdit.css";
 
 const NewAssignment = () => {
   const Navigate = useNavigate();
+  // state hook to take input from form
+  const [inputFields, setInputFields] = useState({
+    name: "",
+    address: "",
+    pinCode: "",
+    jobFunction: "",
+    mobile: "",
+    annualRevenue: "",
+    clientCode: "",
+  });
+
+  // on change capture the input from frontsnd handler
+  const onChangehandler = (e) => {
+    const { name, value } = e.target;
+    setInputFields((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  // handle to submit form data
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(inputFields);
+    // try {
+    //   const response = await axios.post(
+    //     `http://localhost:8000/employee`,
+    //     inputFields,
+    //     {
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //     }
+    //   );
+    //   console.log(response);
+    //   alert("Saved successfully.");
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  };
+
+  // handle to get assingment from backend
+  // useEffect(() => {
+  //   const fetchAssingements =async () =>{
+  //     try {
+  //       const response = await axios.get(``)
+  //       console.log(response)
+
+  //     } catch (error) {
+  //       console.log(error)
+  //     }
+  //   }
+
+  //   fetchAssingements();
+
+  // }, []);
 
   const Previouspage = () => {
     Navigate("/assignment");
@@ -25,51 +85,57 @@ const NewAssignment = () => {
 
   return (
     <Box className="employee-form-container">
-<Box
-      display="grid"
-      gridTemplateColumns="repeat(4, 1fr)"
-      gap={4}
-      p="4"
-      borderWidth="1px"
-      borderRadius="md"
-      mb="4"
-      color="#393e46"
-    >
-      <Box>
-        <Text fontSize="xl">Kaveri Kappor</Text>
-      </Box>
-      <Box>
-        <Text fontSize="xl">4d Apply Ridge Road</Text>
-      </Box>
-      <Box>
-        <Text fontSize="xl">440018</Text>
-      </Box>
-      <Box>
-        <Text fontSize="xl">Vice President</Text>
-      </Box>
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(4, 1fr)"
+        gap={4}
+        p="4"
+        borderWidth="1px"
+        borderRadius="md"
+        mb="4"
+        color="#393e46"
+      >
+        <Box>
+          <Text fontSize="xl">Kaveri Kappor</Text>
+        </Box>
+        <Box>
+          <Text fontSize="xl">4d Apply Ridge Road</Text>
+        </Box>
+        <Box>
+          <Text fontSize="xl">440018</Text>
+        </Box>
+        <Box>
+          <Text fontSize="xl">Vice President</Text>
+        </Box>
 
-      <Divider gridColumn="span 4" my="2" />
+        <Divider gridColumn="span 4" my="2" />
 
-      <Box>
-        <Text fontSize="xl">0000000000</Text>
+        <Box>
+          <Text fontSize="xl">0000000000</Text>
+        </Box>
+        <Box>
+          <Text fontSize="xl">$6 Billion</Text>
+        </Box>
+        <Box>
+          <Text fontSize="xl">CD5B32r3rA9eFPH</Text>
+        </Box>
       </Box>
-      <Box>
-        <Text fontSize="xl">$6 Billion</Text>
-      </Box>
-      <Box>
-        <Text fontSize="xl">CD5B32r3rA9eFPH</Text>
-      </Box>
-    </Box>
       <Box marginBottom={"1rem"} fontSize={"2rem"} fontWeight={"700"}>
         New Assignment
       </Box>
 
-      <form className="employee-form">
+      <form className="employee-form" onSubmit={handleSubmit}>
         <Stack direction={"row"}>
           <Box>
             <FormControl isRequired className="employee-form-group">
               <FormLabel> Name</FormLabel>
-              <Input width={"400px"} type="text" placeholder="Kaveri Kappor" />
+              <Input
+                width={"400px"}
+                type="text"
+                placeholder="Kaveri Kappor"
+                name="name"
+                onChange={onChangehandler}
+              />
             </FormControl>
           </Box>
           <Box>
@@ -79,6 +145,8 @@ const NewAssignment = () => {
                 width={"400px"}
                 type="text"
                 placeholder="4d Apply Ridge Road"
+                name="address"
+                onChange={onChangehandler}
               />
             </FormControl>
           </Box>
@@ -87,13 +155,25 @@ const NewAssignment = () => {
           <Box>
             <FormControl isRequired className="employee-form-group">
               <FormLabel>PinCode</FormLabel>
-              <Input width={"400px"} type="number" placeholder="440018" />
+              <Input
+                width={"400px"}
+                type="number"
+                placeholder="440018"
+                name="pinCode"
+                onChange={onChangehandler}
+              />
             </FormControl>
           </Box>
           <Box>
             <FormControl isRequired className="employee-form-group">
               <FormLabel>Job Function</FormLabel>
-              <Input width={"400px"} type="text" placeholder="Vice President" />
+              <Input
+                width={"400px"}
+                type="text"
+                placeholder="Vice President"
+                name="jobFunction"
+                onChange={onChangehandler}
+              />
             </FormControl>
           </Box>
         </Stack>
@@ -101,13 +181,25 @@ const NewAssignment = () => {
           <Box>
             <FormControl isRequired className="employee-form-group">
               <FormLabel>Mobile</FormLabel>
-              <Input width={"400px"} type="number" placeholder="0000000000" />
+              <Input
+                width={"400px"}
+                type="number"
+                placeholder="0000000000"
+                name="mobile"
+                onChange={onChangehandler}
+              />
             </FormControl>
           </Box>
           <Box>
             <FormControl isRequired className="employee-form-group">
               <FormLabel>Annual Revenue</FormLabel>
-              <Input width={"400px"} type="text" placeholder="$6 Billion" />
+              <Input
+                width={"400px"}
+                type="text"
+                placeholder="$6 Billion"
+                name="annualRevenue"
+                onChange={onChangehandler}
+              />
             </FormControl>
           </Box>
         </Stack>
@@ -115,19 +207,26 @@ const NewAssignment = () => {
         <Box>
           <FormControl isRequired className="employee-form-group">
             <FormLabel>Client Code</FormLabel>
-            <Input width={"400px"} type="text" placeholder="CD5B32r3rA9eFPH" />
+            <Input
+              width={"400px"}
+              type="text"
+              placeholder="CD5B32r3rA9eFPH"
+              name="clientCode"
+              onChange={onChangehandler}
+            />
           </FormControl>
         </Box>
-      </form>
 
-      <Button
-        onClick={Previouspage}
-        className="employee-btn"
-        colorScheme="teal"
-        mt="4"
-      >
-        Submit
-      </Button>
+        <Button
+          // onClick={Previouspage}
+          className="employee-btn"
+          colorScheme="teal"
+          mt="4"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
     </Box>
   );
 };
